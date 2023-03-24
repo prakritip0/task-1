79c7cdcb6dde3739fc8611f88ddf63cf8c634647
Prakriti Pokharel <iprakritip@gmail.com>
2023-03-24 15:03:37 +0545

    feat: complete subscription page

---
+++ js/subscribe.js
@@ -0,0 +1,20 @@
+const modal = document.getElementById("submitModal");
+const submitSubscription = document.getElementById("submitSubscription");
+
+const modalOptYes = document.getElementById("modalYes");
+const modalOptNo = document.getElementById("modalNo");
+
+const emailInput = document.getElementById("emailInput");
+
+
+submitSubscription.addEventListener("click", function () {
+    modal.style.display = "block";
+})
+
+modalOptYes.addEventListener("click", () => {
+    modal.style.display = "none";
+    emailInput.reset();
+})
+modalOptNo.addEventListener("click", () => {
+    modal.style.display = "none";
+})
