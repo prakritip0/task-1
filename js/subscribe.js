092308072d7dec2500ade07afc7ab4a01931581e
Prakriti Pokharel <prakriti.pokharel@globalyhub.com>
2023-03-27 10:55:12 +0545

    feat: add email validation

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,18 +1,31 @@
 const modal = document.getElementById("submitModal");
 const submitSubscription = document.getElementById("submitSubscription");
 
-function subscriptionButton() {
-    modal.style.display = "block";
-}
+// function subscriptionButton() {
+//     modal.style.display = "block";
+// }
 
 const modalOptYes = document.getElementById("modalYes");
 const modalOptNo = document.getElementById("modalNo");
 
-const emailInput = document.getElementById("emailInput");
+const emailInput = document.getElementById("emailAddress");
 
+const validateEmail = () => {
+    const regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+    // console.log(regx.test(emailInput.value));
+    if (regx.test(emailInput.value)) {
+        submitSubscription.disabled = false;
+        modal.style.display = "flex";
+    } else {
+        alert("Please enter a valid email!!");
+        submitSubscription.disabled = true;
+    }
+
+}
 
 submitSubscription.addEventListener("click", function () {
-    modal.style.display = "block";
+    validateEmail();
+    // modal.style.display = "block";
 })
 
 modalOptYes.addEventListener("click", () => {
